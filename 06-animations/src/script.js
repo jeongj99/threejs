90e3a5d0d563ef351fee044587b92cd3bd8808bd
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-05-26 22:38:03 -0400

    Finish lesson 6

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import gsap from 'gsap';
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
@@ -28,4 +29,27 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas
 });
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+
+// Clock
+// const clock = new THREE.Clock();
+
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 });
+
+// Animations
+const tick = () => {
+  // Clock
+  // const elapsedTime = clock.getElapsedTime();
+
+  // // Update objects
+  // camera.position.y = Math.sin(elapsedTime);
+  // camera.position.x = Math.cos(elapsedTime);
+  // camera.lookAt(mesh.position);
+
+  // Render
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(tick);
+};
+
+tick();
